a6e41e578b7b8a14a0ba4a6819ac6b7694997733
jperedadnr <pereda@eii.uva.es>
2015-05-06 19:10:24 +0200

    * Added headers

--- src/org/fxyz/cameras/CameraTransformer.java
+++ src/org/fxyz/cameras/CameraTransformer.java
@@ -1,12 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2013-2015 F(X)yz, 
+ * Sean Phillips, Jason Pollastrini and Jose Pereda
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.fxyz.cameras;
-// 
-// Xform.java is a class that extends the Group class. It is used in the 
+// CameraTransformer is based on Xform, a class that extends the Group class. It is used in the 
 // MoleculeSampleApp application that is built using the Getting Started with JavaFX
 // 3D Graphics tutorial. The method allows you to add your own transforms and rotation.
 // 
--- src/org/fxyz/cameras/controllers/FPSController.java
+++ src/org/fxyz/cameras/controllers/FPSController.java
@@ -1,7 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2013-2015 F(X)yz, 
+ * Sean Phillips, Jason Pollastrini and Jose Pereda
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.fxyz.cameras.controllers;
 
@@ -11,7 +24,6 @@ import javafx.scene.Node;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
-import org.fxyz.geometry.Vector3D;
 import org.fxyz.utils.MathUtils;
 
 /**
